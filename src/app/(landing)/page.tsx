624bf64dbf995350fda2f4a209fff510b2b91929
dough <viktor.melin@me.com>
2024-01-04 21:16:39 +0100

    Move landing page to separate folder

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,49 @@
+import FeatureList from '@/components/FeatureList';
+import Image from 'next/image';
+
+const HomePage = () => {
+  return (
+    <main className="bg-hero-image bg-no-repeat bg-cover text-white p-4">
+      <section className="h-screen w-full flex flex-col items-center justify-center">
+        <header>
+          <h1 className="text-4xl font-medium tracking-tight mb-4">
+            Effortless Expense Management
+          </h1>
+        </header>
+        <p className="text-lg text-gray-200">
+          Say goodbye to the hassle of tracking group expenses! Billbuddies is
+          here to take the pain out of splitting bills. Whether it&apos;s rent
+          with roommates, a dinner with friends, or shared vacation costs,
+          Billbuddies makes it simple.
+        </p>
+        <button className="mt-16 w-full inline-flex justify-center rounded-xl py-3 px-3 text-sm font-semibold outline-2 outline-offset-2 transition-colors bg-brand-purple-normal text-white hover:bg-brand-purple-darker active:bg-brand-purple-dark active:text-white/80">
+          Get Started today
+        </button>
+      </section>
+      <section className="min-h-screen w-full flex flex-col items-center">
+        <header className="mb-4">
+          <h2 className="text-3xl font-medium tracking-tight mb-4">
+            Features Designed with You in Mind
+          </h2>
+          <FeatureList />
+        </header>
+      </section>
+      <footer className="mt-16 w-full flex flex-col items-center">
+        <a
+          className="py-3 px-3 text-sm uppercase tracking-tigth"
+          href="/contact"
+        >
+          Contact Us
+        </a>
+        <a
+          className="py-3 px-3 text-sm uppercase tracking-tigth"
+          href="/contact"
+        >
+          Privacy Policy & Terms of Service
+        </a>
+      </footer>
+    </main>
+  );
+};
+
+export default HomePage;
