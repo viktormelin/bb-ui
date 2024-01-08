b9a42bad4435d06814b073fd53ebf9575383e4ff
viktormelin <viktor.melin@me.com>
2024-01-08 17:04:40 +0100

    Separate footer into own component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 w-full flex items-center justify-evenly">
+      <Link className="p-3 text-xs uppercase tracking-tigth" href="/contact">
+        Contact Us
+      </Link>
+      <Link className="p-3 text-xs uppercase tracking-tigth" href="/contact">
+        Privacy & Terms
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
