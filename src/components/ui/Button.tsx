12e63aa291b30930c54f469f8d1775882da5feea
viktormelin <viktor.melin@me.com>
2024-01-08 17:04:28 +0100

    Add neutral color theme to button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,12 +3,14 @@ import { VariantProps, cva } from 'class-variance-authority';
 import React, { ButtonHTMLAttributes } from 'react';
 
 const buttonVariants = cva(
-  'w-full inline-flex justify-center rounded-xl py-3 px-3 text-sm font-semibold outline-2 outline-offset-2 transition-colors',
+  'w-full inline-flex justify-center items-center rounded-xl p-3 font-semibold outline-2 outline-offset-2 transition-colors disabled:cursor-disabled disabled:text-neutral-400',
   {
     variants: {
       variant: {
         primary:
-          'w-full inline-flex justify-center rounded-xl py-3 px-3 text-sm font-semibold outline-2 outline-offset-2 transition-colors bg-brand-purple-normal text-white hover:bg-brand-purple-darker active:bg-brand-purple-dark active:text-white/800',
+          'bg-brand-purple-normal text-white hover:bg-brand-purple-darker active:bg-brand-purple-dark active:text-white/800',
+        neutral:
+          'bg-neutral-100 text-black hover:bg-neutral-300 active:bg-neutral-300 active:text-black/800',
       },
     },
     defaultVariants: {
