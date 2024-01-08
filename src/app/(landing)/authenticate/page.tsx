0328c1416ef589f09993085f4d972893df776083
viktormelin <viktor.melin@me.com>
2024-01-08 17:05:08 +0100

    Create login / authentication page

---
+++ src/app/(landing)/authenticate/page.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Button from '@/components/ui/Button';
+import { useAuthorizer } from '@authorizerdev/authorizer-react';
+import Image from 'next/image';
+
+const AuthPage = () => {
+  const { authorizerRef } = useAuthorizer();
+
+  const oauthLoginClick = (provider: string) => {
+    authorizerRef.oauthLogin(provider);
+  };
+
+  return (
+    <main className="h-screen w-full flex flex-col items-center justify-center p-4">
+      <header>
+        <h1 className="text-4xl font-medium tracking-tight mb-4">Login</h1>
+      </header>
+      <p className="text-lg text-gray-200">
+        Ready to take control of your group expenses? Continue now and
+        experience the easiest way to manage shared finances
+      </p>
+      <div className="w-full flex flex-col gap-4 mt-16">
+        <Button
+          variant="neutral"
+          className="flex gap-3"
+          onClick={() => oauthLoginClick('google')}
+        >
+          <Image
+            className=""
+            src="/images/google_icon.png"
+            width={16}
+            height={16}
+            alt="Google"
+          />
+          <span>Continue with Google</span>
+        </Button>
+        <Button
+          disabled
+          variant="neutral"
+          className="flex gap-3"
+          onClick={() => oauthLoginClick('apple')}
+        >
+          <Image
+            className=""
+            src="/images/apple_icon.svg"
+            width={16}
+            height={16}
+            alt="Apple"
+          />
+          <span>Continue with Apple</span>
+        </Button>
+      </div>
+    </main>
+  );
+};
+
+export default AuthPage;
