d1ba6da024c239caaff5994cde8a31a90d1a501e
viktormelin <viktor.melin@me.com>
2024-01-09 13:44:55 +0100

    Create mobile nav with provider

---
+++ src/components/Dashboard/Navbar/MobileNav.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { cn } from '@/lib/clientUtils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { dashboardLinks } from './NavProvider';
+import Image from 'next/image';
+import { RectangleGroupIcon } from '@heroicons/react/24/solid';
+import { BanknotesIcon } from '@heroicons/react/24/solid';
+import { PlusCircleIcon } from '@heroicons/react/24/solid';
+import { UserGroupIcon } from '@heroicons/react/24/solid';
+import { UserIcon } from '@heroicons/react/24/solid';
+
+const isCurrentPage = (pathname: string, target: string) => {
+  const paths = pathname.split('/');
+  const targets = target.split('/');
+
+  const lastPath = paths[paths.length - 1];
+  const lastTarget = targets[targets.length - 1];
+
+  if (lastPath === lastTarget) return true;
+  return false;
+};
+
+const MobileNav = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="fixed bottom-0 left-0 w-full flex items-center justify-between p-4 border-t border-neutral-200">
+      <Link
+        href={dashboardLinks[0].href}
+        className={cn(
+          'p-3 rounded-lg transition-all',
+          isCurrentPage(pathname, dashboardLinks[0].href)
+            ? 'text-brand-purple-normal border-b shadow-md'
+            : 'hover:text-brand-purple-normal',
+        )}
+      >
+        <RectangleGroupIcon className="w-6 h6" />
+      </Link>
+      <Link
+        href={dashboardLinks[1].href}
+        className={cn(
+          'p-3 rounded-lg transition-all',
+          isCurrentPage(pathname, dashboardLinks[1].href)
+            ? 'text-brand-purple-normal shadow-md'
+            : 'hover:text-brand-purple-normal',
+        )}
+      >
+        <BanknotesIcon className="w-6 h6" />
+      </Link>
+      <Link
+        href={dashboardLinks[2].href}
+        className={cn(
+          'p-3 rounded-lg transition-all bg-brand-purple-normal text-white',
+          isCurrentPage(pathname, dashboardLinks[2].href)
+            ? 'text-brand-purple-normal shadow-md'
+            : 'hover:text-neutral-400',
+        )}
+      >
+        <PlusCircleIcon className="w-6 h6" />
+      </Link>
+      <Link
+        href={dashboardLinks[3].href}
+        className={cn(
+          'p-3 rounded-lg transition-all',
+          isCurrentPage(pathname, dashboardLinks[3].href)
+            ? 'text-brand-purple-normal shadow-md'
+            : 'hover:text-brand-purple-normal',
+        )}
+      >
+        <UserGroupIcon className="w-6 h6" />
+      </Link>
+      <Link
+        href={dashboardLinks[4].href}
+        className={cn(
+          'p-3 rounded-lg transition-all',
+          isCurrentPage(pathname, dashboardLinks[4].href)
+            ? 'text-brand-purple-normal shadow-md'
+            : 'hover:text-brand-purple-normal',
+        )}
+      >
+        <UserIcon className="w-6 h6" />
+      </Link>
+    </nav>
+  );
+};
+
+export default MobileNav;
---
+++ src/components/Dashboard/Navbar/NavProvider.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import MobileNav from './MobileNav';
+
+export const dashboardLinks = [
+  {
+    href: '/dashboard',
+  },
+  {
+    href: '/dashboard/expenses',
+  },
+  {
+    href: '/dashboard/expenses/add',
+  },
+  {
+    href: '/dashboard/groups',
+  },
+  {
+    href: '/dashboard/profile',
+  },
+];
+
+const NavProvider = () => {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+
+  if (isMobile) {
+    return <MobileNav />;
+  }
+};
+
+export default NavProvider;
