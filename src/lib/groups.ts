d5a827a08e8644b7bdd6aac1d937393b899f6118
viktormelin <viktor.melin@me.com>
2024-01-13 22:53:00 +0100

    Add server action to create group

--- src/lib/groups.ts
+++ src/lib/groups.ts
@@ -1,3 +1,6 @@
+'use server';
+
+import { redirect } from 'next/navigation';
 import { generateAuthHeaders } from './authorizer';
 
 export const getUserGroups = async () => {
@@ -9,4 +12,21 @@ export const getUserGroups = async () => {
 
   if (response.ok) return await response.json();
   return null;
+};
+
+export const createGroup = async (data: any) => {
+  const headers = await generateAuthHeaders();
+  const response = await fetch(`${process.env.API_ROUTE}/groups/new`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(data),
+  });
+
+  const body = await response.json();
+
+  if (response.ok) {
+    redirect(`/dashboard/expenses/${body.group.id}`);
+  } else {
+    console.error(body.error);
+  }
 };
