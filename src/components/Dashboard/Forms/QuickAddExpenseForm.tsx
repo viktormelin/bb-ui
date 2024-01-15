672cbdbf3b372f037c4ee65759bbfc910f72890d
viktormelin <viktor.melin@me.com>
2024-01-15 17:06:53 +0100

    Update expense / group creation to exclude users

--- src/components/Dashboard/Forms/QuickAddExpenseForm.tsx
+++ src/components/Dashboard/Forms/QuickAddExpenseForm.tsx
@@ -4,14 +4,11 @@ import LoadingSpinner from '@/components/LoadingSpinner';
 import Button from '@/components/ui/Button';
 import ComboBox, { IComboBoxData } from '@/components/ui/ComboBox';
 import Input from '@/components/ui/Input';
-import MultiBox from '@/components/ui/MultiBox';
-import Text from '@/components/ui/Text';
 import { createGroup } from '@/lib/groups';
 import { IUserGroup } from '@/types/Groups';
 import { IUserProfile } from '@/types/User';
 import { useAuthorizer } from '@authorizerdev/authorizer-react';
-import { MinusCircleIcon } from '@heroicons/react/24/solid';
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 interface IProps {
   groups: IUserGroup[];
@@ -19,7 +16,7 @@ interface IProps {
 }
 
 const QuickAddExpenseForm = ({ groups, friends }: IProps) => {
-  const { user, loading } = useAuthorizer();
+  const { loading } = useAuthorizer();
 
   const mappedGroups: IComboBoxData[] = groups.map((group) => ({
     id: group.groups.id,
@@ -28,53 +25,20 @@ const QuickAddExpenseForm = ({ groups, friends }: IProps) => {
 
   const defaultGroup = mappedGroups.length > 0 ? mappedGroups[0].name : '';
   const [selectedGroup, setSelectedGroup] = useState<string>(defaultGroup);
-  const [selectedFriends, setSelectedFriends] = useState<IComboBoxData[]>([]);
-
   const [expenseName, setExpenseName] = useState('');
   const [expenseAmount, setExpenseAmount] = useState('');
   const [creatingGroup, setCreatingGroup] = useState(false);
-
-  useEffect(() => {
-    if (user && !loading) {
-      setSelectedFriends([
-        {
-          id: String(0),
-          name: user.email,
-        },
-      ]);
-    }
-  }, [loading]);
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setCreatingGroup(true);
     const body = {
       group: selectedGroup,
-      users: selectedFriends,
-      expense: { name: expenseName, money_total: expenseAmount },
+      expense: { name: expenseName, expense_total: expenseAmount },
     };
 
     createGroup(body);
     setCreatingGroup(false);
-  };
-
-  const handleSelectNewFriend = (values: IComboBoxData[]) => {
-    for (const value of values) {
-      if (selectedFriends.some((s) => s.name === value.name)) continue;
-
-      setSelectedFriends([
-        ...selectedFriends,
-        {
-          id: value.id,
-          name: value.name,
-        },
-      ]);
-    }
-  };
-
-  const removeFriend = (email: string) => {
-    if (email === user?.email) return;
-    setSelectedFriends(selectedFriends.filter((s) => s.name !== email));
   };
 
   if (loading) return <LoadingSpinner />;
@@ -88,13 +52,6 @@ const QuickAddExpenseForm = ({ groups, friends }: IProps) => {
         selected={selectedGroup}
         setSelected={setSelectedGroup}
       />
-      <MultiBox
-        data={selectedFriends}
-        label="Add users (email)"
-        createNew="Invite"
-        selected={selectedFriends}
-        setSelected={handleSelectNewFriend}
-      />
       <Input
         label="Expense name"
         value={expenseName}
@@ -106,32 +63,6 @@ const QuickAddExpenseForm = ({ groups, friends }: IProps) => {
         value={expenseAmount}
         onChange={(e) => setExpenseAmount(e.target.value)}
       />
-      <ul className="flex flex-col gap-2 my-4 p-2 bg-neutral-100 rounded-lg">
-        <Text variant="h3">Users</Text>
-        {selectedFriends.map((item) => (
-          <li
-            key={item.name}
-            className="rounded-lg w-full flex justify-between items-center"
-          >
-            <div>
-              <Text variant="xs" className="mb-0">
-                {item.name}
-              </Text>
-            </div>
-            <div className="flex items-center">
-              <Button
-                type="button"
-                variant="neutral"
-                className="p-0"
-                disabled={item.name === user?.email}
-                onClick={() => removeFriend(item.name)}
-              >
-                <MinusCircleIcon className="w-5 h-5" />
-              </Button>
-            </div>
-          </li>
-        ))}
-      </ul>
       <Button disabled={creatingGroup}>Create expense</Button>
     </form>
   );
