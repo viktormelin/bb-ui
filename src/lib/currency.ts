ddb00e4420e0e2f5d2376b2b0a2bdf3712e3681a
viktormelin <viktor.melin@me.com>
2024-01-15 17:07:12 +0100

    Create function to calculate total amount for expense group

--- src/lib/currency.ts
+++ src/lib/currency.ts
@@ -1,4 +1,12 @@
+import { IGroupExpenses } from '@/types/Groups';
+
 export const formatCurrency = (num: number) =>
   new Intl.NumberFormat('se-SE', { style: 'currency', currency: 'SEK' }).format(
     num,
   );
+
+export const calculateTotal = (arr: IGroupExpenses[]) => {
+  let total = 0;
+  arr.map((item) => (total += item.expense_total));
+  return total;
+};
