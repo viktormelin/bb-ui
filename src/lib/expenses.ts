d3e67d4f7734590b68cdf5253f3c3269bbfa5ffa
viktormelin <viktor.melin@me.com>
2024-01-22 13:58:16 +0100

    Fix issue with response from expense fetch

--- src/lib/expenses.ts
+++ src/lib/expenses.ts
@@ -6,9 +6,16 @@ export const getUserExpenses = async () => {
   const headers = await generateAuthHeaders();
   const response = await fetch(`${process.env.API_ROUTE}/expenses`, {
     headers,
-    cache: 'no-store',
   });
 
-  if (response.ok) return await response.json();
+  console.log(response);
+
+  if (response.ok) {
+    const body = (await response.json()) ?? [];
+    console.log(body);
+
+    return body;
+  }
+
   return null;
 };
