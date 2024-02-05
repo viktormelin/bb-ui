01f2c2cd53a493a26a28922a604ad97e300bcbbb
viktormelin <viktor.melin@me.com>
2024-02-05 15:46:25 +0100

    Update dashboard with useful information

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -4,11 +4,6 @@ import { formatCurrency } from '@/lib/currency';
 import { getUserExpenses } from '@/lib/expenses';
 import { IExpenseSplit } from '@/types/Expense';
 
-const mockData = {
-  money_outstanding: 1235,
-  money_owed: 311,
-};
-
 const fetchUserData = async () => {
   return await getUserData();
 };
@@ -24,47 +19,43 @@ const DashboardPage = async () => {
   const expensesData = fetchExpenses();
   const [user, expenses] = await Promise.all([userData, expensesData]);
 
+  const totMoneyOutstanding = expenses.reduce((prev, curr) => {
+    if (!curr.expense.settled) {
+      return prev + curr.amount;
+    } else {
+      return prev + 0;
+    }
+  }, 0);
+
+  const totMoneyTransactions = expenses.reduce((prev, curr) => {
+    if (!curr.expense.settled) {
+      return prev + curr.expense.expense_total;
+    } else {
+      return prev + 0;
+    }
+  }, 0);
+
   return (
     <>
       <section className="w-full">
         <header>
           <Text type="h1">Hi {user.given_name}</Text>
-          <Text>View your current balance and latest transactions</Text>
+          <Text>View your current balance</Text>
         </header>
         <div className="flex flex-col gap-3 my-4">
           <div className="rounded-lg border border-neutral-200">
             <div className="p-5">
-              <Text variant="xs">Total money outstanding</Text>
-              <Text variant="3xl">
-                {formatCurrency(mockData.money_outstanding)}
-              </Text>
+              <Text variant="xs">Total money outstanding*</Text>
+              <Text variant="3xl">{formatCurrency(totMoneyOutstanding)}</Text>
             </div>
           </div>
           <div className="rounded-lg border border-neutral-200">
             <div className="p-5">
-              <Text variant="xs">Total money owed</Text>
-              <Text variant="3xl">{formatCurrency(mockData.money_owed)}</Text>
+              <Text variant="xs">Total money in transactions</Text>
+              <Text variant="3xl">{formatCurrency(totMoneyTransactions)}</Text>
             </div>
           </div>
         </div>
-      </section>
-      <section className="border-t border-neutral-200">
-        <header>
-          <Text type="h2" className="pt-2">
-            Latest Transactions
-          </Text>
-        </header>
-        <ul className="flex flex-col items-center gap-2 my-4">
-          <li className="rounded-lg w-full flex justify-between items-center">
-            <div>
-              <Text variant="xl">Transaction Name</Text>
-              <Text variant="xs">Transaction Group right here</Text>
-            </div>
-            <div>
-              <Text variant="xl">+ {formatCurrency(42)}</Text>
-            </div>
-          </li>
-        </ul>
       </section>
     </>
   );
