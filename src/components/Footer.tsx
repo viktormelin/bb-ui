65ee3e3d899cf2e386288fc07f4dfd3fa45bc0f1
viktormelin <viktor.melin@me.com>
2024-02-09 12:12:36 +0100

    Update footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,14 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className="absolute bottom-0 w-full flex items-center justify-evenly">
-      <Link className="p-3 text-xs uppercase tracking-tigth" href="/contact">
+      {/* <Link className="p-3 text-xs uppercase tracking-tigth" href="/contact">
         Contact Us
       </Link>
       <Link className="p-3 text-xs uppercase tracking-tigth" href="/contact">
         Privacy & Terms
+      </Link> */}
+      <Link className="p-3 text-xs uppercase tracking-tigth" href="/cookies">
+        Cookies 🍪
       </Link>
     </footer>
   );
